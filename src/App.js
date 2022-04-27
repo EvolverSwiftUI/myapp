b6753fe1596fc48dadf72bf0e23c14014ce3831f
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2022-04-27 19:13:13 +0530

    aaddons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import UserProfile from './components/UserProfile';
 
-const userDetails = {
-  imageUrl: "https://assets.ccbp.in/frontend/react-js/esther-howard-img.png",
-  name: "Sivaram",
-  role: "Software Developer"
-}
+const userDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Esther Howard',
+    role: 'Software Developer'
+  },
+  {
+    uniqueNo: 2,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'Floyd Miles',
+    role: 'Software Developer'
+  },
+  {
+    uniqueNo: 3,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'Jacob Jones',
+    role: 'Software Developer'
+  },
+  {
+    uniqueNo: 4,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/esther-devon-lane.png',
+    name: 'Devon Lane',
+    role: 'Software Developer'
+  }
+]
 
 // function App() {
 //   return (
@@ -30,6 +55,18 @@ const userDetails = {
 // }
 
 // const App = () => <h1>Users List App</h1>
-const App = () => <UserProfile />
+// const App = () => <UserProfile userDetails={userDetails}/>
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">Uses List</h1>
+    <ul>
+        {
+          userDetailsList.map((eachItem) =>(
+            <UserProfile userDetails={eachItem} key={eachItem.uniqueNo}/>
+          ))
+        }
+    </ul>
+  </div>
+)
 
 export default App;
--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -1,9 +1,19 @@
 import React from "react";
+import './index.css'
 
-const UserProfile = () => (
-    <div>
-        <img src="https://assets.ccbp.in/frontend/react-js/esther-howard-img.png" />
-    </div>
-)
+const UserProfile = (props) => {
+const {userDetails} = props;
+const {imageUrl, name, role} = userDetails;
+    return (
+        <li className="user-card-container">
+            {/* <img src="https://assets.ccbp.in/frontend/react-js/esther-howard-img.png" /> */}
+            <img src={imageUrl} alt="avatar" className="avatar"/>
+            <div className="user-details-container">
+                <h1 className="user-name">{name}</h1>
+                <p className="user-designation">{role}</p>
+            </div>
+        </li>
+    )
+}
 
 export default UserProfile;
